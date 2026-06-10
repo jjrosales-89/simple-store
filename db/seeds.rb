500a853058190bef80c7667ffdd05f0516da80fe
Juan Jose R.O. <jrosalesortega@academic.rrc.ca>
2026-06-10 14:42:26 -0500

    Seed products with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.destroy_all
+
+676.times do
+  Product.create!(
+    title: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 1.0..100.0),
+    stock_quantity: Faker::Number.between(from: 0, to: 100)
+  )
+end
+
+puts "Created #{Product.count} products."
